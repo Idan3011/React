9e6b2f96299150e727bb864a119407b8cafe6d41
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-11-23 13:53:52 +0200

    Module of today 23.11.2023

---
+++ 5.1_buttons/src/assets/script.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+export const Buttons = ({bt1, btn2}) => {
+  return (
+    <>
+    <button style={{fontWeight: 'bold'}} >Importent</button>
+    <button style={{fontWeight: 'light'}} >Importent</button>
+    </>
+    
+  )
+}
