2a54d57f16f4c2cc9567ecff0dd1a883b673bab4
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-11-27 11:19:13 +0200

    Mudole of today 26.11.2023

---
+++ 7.2_checkbox/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const check = ['I read the term of the app', 'I accept the trem of the app',
+                 'I want to get the weekly news letter', 'I want to get sales and offer']
+  const [checked, setCheck] = useState(true)
+
+    function Checkbox(){
+      
+
+      return(
+        <>
+        
+        {check.map((element, index) =>{
+            if(index ? index==1: index==0) {
+            return(
+              <div key={index}>
+             <input  type="checkbox"  defaultChecked={!checked}  name={index}/>
+             <label htmlFor={index}>{element}</label>
+             </div>
+        )}  else {
+          return( 
+          <div key={index}>
+             <input  type="checkbox"  defaultChecked={checked}  name={index}/>
+             <label htmlFor={index}>{element}</label>
+             </div>)}       
+        })}
+        </>
+      ) 
+    }
+
+  return (
+    <>
+    <div className="container">
+    <Checkbox />
+    </div>
+    
+    </>
+  )
+}
+
+export default App
