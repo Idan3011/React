dec82dad4d84c5c7c59d6de9d8185ea39b416ebc
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-12-12 00:29:40 +0200

    Module of today 11/12/2023

--- Module_of_today_11_12_2023/src/App.jsx
+++ Module_of_today_11_12_2023/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Ex_20_1 from './assets/pages/Ex_20.1/Ex_20_1'
-
+import Ex_20_2 from './assets/pages/Ex_20.2/Ex_20_2'
 function App() {
  
 
   return (
     <>
      <div className="App">
-      <Ex_20_1 />
+      {/* <Ex_20_1 /> */}
+      <Ex_20_2 />
      </div>
     </>
   )
---
+++ Module_of_today_11_12_2023/src/assets/pages/Ex_20.2/Ex_20_2.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import "./EX_20_2.css";
+export default class Ex_20_2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: "CSS", completed: true },
+        { name: "JavaScript", completed: true },
+        { name: "Learn React", completed: false },
+        { name: "Learn mongoDB", completed: false },
+        { name: "Learn Node JS", completed: false },
+      ],
+    };
+  }
+  handleClick = (toDo, index) => {
+    if (toDo === this.state.data[index].completed && toDo == false) {
+      this.setState((prev) => ({
+        [this.state.data[index]]: (prev.data[index].completed = !false),
+      }));
+    }
+    if(toDo === this.state.data[index].completed && toDo == true){
+   this.setState((prev)=>({
+    [this.state.data[index]]: (prev.data[index].completed = !true)
+   }));
+   
+  }
+  };
+  render() {
+    const { name, completed } = this.props;
+    const { data } = this.state;
+    return (
+      <div className="Ex">
+        <div className="Ex_20_2">
+          <h1>Ex_20_2</h1>
+          <ul>
+            {data.map((toDo, index) => {
+              return (
+                <li
+                  key={index}
+                  onClick={() => this.handleClick(toDo.completed, index)}
+                  className={toDo.completed ? "complete" : null}
+                >
+                  {toDo.name} {toDo.completed ? "V" : "X"}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+// this.setState((status) => ({
+//   [this.State.data[index]]: (status.data[index].completed = !true),
+// }));
